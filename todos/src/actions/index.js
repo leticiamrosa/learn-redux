cbeefcd93fcb24a8fa5cd7175906c03baf768a71
Leticia Monteiro <leticiamonteiro.design@gmail.com>
2018-09-09 23:08:54 -0300

    Implementation action toggle todo

--- todos/src/actions/index.js
+++ todos/src/actions/index.js
@@ -1,11 +1,13 @@
 // boas praticas usar aqui apenas para exportas as actions
+// Sao as acoes que o usuario ira tomar
 
-const ADD_TODO = 'ADD_TODO';
+export const ADD_TODO = 'ADD_TODO';
 // export const addTodo = text => ({ // funcao que recebe o que a action precisa pra funcionar
 //     type: ADD_TODO,
 //     text: text // equivalente a text: text | key:value
 // });
 
+// Adicionar nova lista
 export const addTodo = (text) => {
     return {
         type: ADD_TODO,
@@ -13,3 +15,9 @@ export const addTodo = (text) => {
     }
 }
 
+// Marcar done
+export const TOGGLE_TODO = 'TOGGLE_TODO'
+export const toggleTodo = todoId => ({
+    type: TOGGLE_TODO,
+    todoId
+});
--- todos/src/reducers/todoListReducer.js
+++ todos/src/reducers/todoListReducer.js
@@ -5,7 +5,6 @@ let nextId = 1;
 const todoListReducer = (state = [], action) => {
     switch(action.type) {
         case 'ADD_TODO':
-            console.log('addTodo foi chamado', action.text)
             const newTodo = {
                 id: nextId++,
                 text: action.text
